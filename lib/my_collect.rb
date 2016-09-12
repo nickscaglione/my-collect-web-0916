e75d1e5f74bb77d7036d325eace0f4ad55c60143
Nick Scaglione <nicholas.a.scaglione@gmail.com>
2016-09-12 21:16:38 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,9 @@
-
-
+def my_collect(collection)
+  counter = 0
+  new_collection = Array.new
+  while counter < collection.size do
+    new_collection << yield(collection[counter])
+    counter += 1
+  end
+  new_collection
+end
